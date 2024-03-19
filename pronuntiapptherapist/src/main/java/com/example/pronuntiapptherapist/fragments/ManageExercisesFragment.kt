9a5462d1e157d2c68c51293b7358e6109a5dbfd5
booleanadv97 <staff_badgame@yahoo.com>
2024-03-19 15:05:47 +0100

    - Removed unused imports

--- pronuntiapptherapist/src/main/java/com/example/pronuntiapptherapist/fragments/ManageExercisesFragment.kt
+++ pronuntiapptherapist/src/main/java/com/example/pronuntiapptherapist/fragments/ManageExercisesFragment.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridView
 import androidx.fragment.app.Fragment
 import com.example.pronuntiapptherapist.R
 import com.example.pronuntiapptherapist.databinding.FragmentManageExercisesBinding
 import com.example.pronuntiapptherapist.fragments.AudioExercise.ManageAudioExercisesFragment
 import com.example.pronuntiapptherapist.fragments.ImageExercise.ManageImageExercisesFragment
 import com.example.pronuntiapptherapist.fragments.ImageRecognitionExercise.ManageImageRecognitionExercisesFragment
-import com.google.firebase.database.FirebaseDatabase
 
 
 class ManageExercisesFragment : Fragment() {
